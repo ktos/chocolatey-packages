b6fe19e4c36021b693c4d0e6c0886ad8496fbbce
Marcin Badurowicz <ktos@ktos.info>
2014-07-28 21:40:20 +0200

    Chocolatey package for brewtarget

---
+++ brewtarget/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'brewtarget'
+$installerType = 'EXE'
+$url = 'https://launchpad.net/brewtarget/trunk/2.0.3/+download/brewtarget_2.0.3_x86.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
