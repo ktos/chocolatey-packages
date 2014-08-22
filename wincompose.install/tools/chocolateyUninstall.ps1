d44cf7628a1bf3a1788830497730810a3cc787c2
Marcin Badurowicz <ktos@ktos.info>
2014-08-22 14:02:21 +0200

    very basic uninstallation script

---
+++ wincompose.install/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'wincompose.install'
+$installerType = 'EXE'
+$silentArgs = '/VERYSILENT /NORESTART'
+$validExitCodes = @(0)
+
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$uninstallpath = ${Env:ProgramFiles(x86)} + "\WinCompose"
+if (Test-Path $uninstallPath) {	
+	$uninstallFilePath = "$uninstallPath\unins000.exe"
+	Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $uninstallFilePath
+} else {
+	Write-Warning "$packageName uninstaller not found. Uninstalling only package from Chocolatey."
+}
