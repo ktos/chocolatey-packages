20e9d376da0ef0f583993d44cf6d7850a83ae28a
Marcin Badurowicz <ktos@ktos.info>
2014-08-22 12:37:10 +0200

    nuspeces for wincompose

---
+++ wincompose.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'wincompose.install'
+$installerType = 'EXE'
+$url = 'https://github.com/samhocevar/wincompose/releases/download/v0.6.11/WinCompose-Setup-0.6.11.exe'
+$silentArgs = '/SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ wincompose.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'wincompose.portable'
+$installerType = 'EXE'
+$url = 'https://github.com/samhocevar/wincompose/releases/download/v0.6.11/WinCompose-NoInstall-0.6.11.zip'
+$validExitCodes = @(0)
+
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+Install-ChocolateyZipPackage $packageName $url $installDir
+Write-ChocolateySuccess "$packageName"
