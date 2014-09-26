43cc3839d36f847dea0d343777085b1da1b68ec8
Marcin Badurowicz <ktos@ktos.info>
2014-09-26 20:13:04 +0200

    uninstall script

---
+++ brewtarget/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'brewtarget'
+$installerType = 'EXE'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$uninstallpath = ${Env:ProgramFiles(x86)} + "\BrewTarget-2.1.0"
+if (Test-Path $uninstallPath) {	
+	$uninstallFilePath = "$uninstallPath\Uninstall.exe"
+	Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $uninstallFilePath
+} else {
+	Write-Warning "$packageName uninstaller not found. Uninstalling only package from Chocolatey."
+}
