0e84fbacbee7a7eecda6b8c0cab84c361a2c46cd
Marcin Badurowicz <m@badurowicz.net>
2015-05-03 19:45:36 +0200

    initial revision for top_netinfo

---
+++ top_netinfo/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'top_netinfo.portable'
+$url = 'http://www.sklepy.ehost.pl/top_netinfo.zip'
+
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+Install-ChocolateyZipPackage $packageName $url $installDir
+Write-ChocolateySuccess "$packageName"
