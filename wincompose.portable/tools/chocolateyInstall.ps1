3bc02daaf7823516165bcc3970464c0a5f02a889
Marcin Badurowicz <m@badurowicz.net>
2016-05-23 18:25:26 +0200

    fixes for new guidelines and fixed version being downloaded

--- wincompose.portable/tools/chocolateyInstall.ps1
+++ wincompose.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 ﻿$packageName = 'wincompose.portable'
 $installerType = 'EXE'
-$url = 'https://github.com/samhocevar/wincompose/releases/download/v0.7.4/WinCompose-NoInstall-0.7.4.zip'
+$url = 'https://github.com/samhocevar/wincompose/releases/download/v0.7.6/WinCompose-NoInstall-0.7.6.zip'
 $validExitCodes = @(0)
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 Install-ChocolateyZipPackage $packageName $url $installDir
-Write-ChocolateySuccess "$packageName"
