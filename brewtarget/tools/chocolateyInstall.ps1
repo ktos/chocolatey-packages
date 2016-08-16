e04be830251807519d33146bd06dad79ed86b6ca
Marcin Badurowicz <m@badurowicz.net>
2016-08-16 16:01:31 +0200

    checksum added for brewtarget

--- brewtarget/tools/chocolateyInstall.ps1
+++ brewtarget/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ $url = 'https://launchpad.net/brewtarget/trunk/2.3.0/+download/brewtarget_2.3.0_
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -ValidExitCodes $validExitCodes -Checksum "043F7BF1BE31C0AD36DAE102F64B03C30528DDDEF5A087CC203317F067048E4B" -ChecksumType "sha256"
