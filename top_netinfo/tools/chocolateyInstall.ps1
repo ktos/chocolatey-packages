bb505bb03001226311ff755e8a26e1750373eefd
Marcin Badurowicz <m@badurowicz.net>
2016-08-17 18:13:14 +0200

    removing Write-ChocolateySuccess

--- top_netinfo/tools/chocolateyInstall.ps1
+++ top_netinfo/tools/chocolateyInstall.ps1
@@ -3,4 +3,3 @@ $url = 'http://www.sklepy.ehost.pl/top_netinfo.zip'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 Install-ChocolateyZipPackage $packageName $url $installDir
-Write-ChocolateySuccess "$packageName"
