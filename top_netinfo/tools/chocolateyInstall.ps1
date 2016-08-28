c5f80c251d33c51bac69298f5c1303f432871040
Marcin Badurowicz <m@badurowicz.net>
2016-08-28 11:11:11 +0200

    removed portable suffix, added checksum, new project url

--- top_netinfo/tools/chocolateyInstall.ps1
+++ top_netinfo/tools/chocolateyInstall.ps1
@@ -1,5 +1,7 @@
-﻿$packageName = 'top_netinfo.portable'
+﻿$packageName = 'top_netinfo'
 $url = 'http://www.sklepy.ehost.pl/top_netinfo.zip'
 
+$checksum = '028929B175C0486ED86F14D525222708D3BD3B4E60CC3899897B7C5D3B3866FB'
+
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-Install-ChocolateyZipPackage $packageName $url $installDir
+Install-ChocolateyZipPackage $packageName $url $installDir -Checksum $checksum -ChecksumType 'sha256'
